5ab83195a94a7f9e514e47902bf325319fb828d8
isaidakbar <smahmudxojaev@gmail.com>
2022-04-26 14:14:13 +0500

    modifies: s

---
+++ figma.config.js
@@ -0,0 +1,29 @@
+// figma.config.js
+require("dotenv").config();
+const svgo = require('@figma-export/transform-svg-with-svgo')
+
+const fileId = process.env.FILE_ID;
+const outputters = [
+  require("@figma-export/output-components-as-svg")({ output: "./" })
+];
+
+/** @type {import('svgo').PluginConfig[]} */
+const solidSVGOConfig = [
+  { removeDimensions: true },
+  { sortAttrs: true },
+  { removeAttrs: { attrs: "fill" } },
+  { addAttributesToSVGElement: { attribute: { fill: "currentColor" } } },
+];
+
+/** @type {import('@figma-export/types').FigmaExportRC} */
+module.exports = {
+  commands: [
+    ["components", {
+        fileId,
+        onlyFromPages: ["solid"],
+        transformers: [svgo({ multipass: true, plugins: solidSVGOConfig })],
+        outputters,
+      },
+    ]
+  ],
+};
--- index.js
+++ index.js
@@ -1,4 +1,4 @@
 const HomeFilled = require('./icons/HomeFilled.svg')
 const HomeOutlined = require('./icons/HomeOutlined.svg')
-
+console.log(HomeOutlined)
 module.exports = { HomeFilled, HomeOutlined }
